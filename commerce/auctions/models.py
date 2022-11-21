4192cd6084302f8ccbe8a218f29191f0eee3b403
chia7812033 <51458240+chia7812033@users.noreply.github.com>
2022-11-22 00:54:59 +0800

    List all items

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -3,4 +3,14 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    id = models.AutoField(primary_key=True)
+
+class Item(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=128)
+    price = models.DecimalField(max_digits=19, decimal_places=10)
+    create_date = models.DateField(auto_now=False, auto_now_add=True)
+    create_time = models.TimeField(auto_now=False, auto_now_add=True)
+    description = models.TextField()
+    image = models.ImageField(upload_to="items/")
+    
